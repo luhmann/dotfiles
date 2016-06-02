2130589259363472ac7f9bc7e25c986dbece56ab
Jan Florian Dietrich <jan-florian.dietrich@epost-dev.de>
2016-06-02 08:49:35 +0200

    fix: set certificate generation to localhost

--- .config/fish/functions/genkey.fish
+++ .config/fish/functions/genkey.fish
@@ -1,3 +1,3 @@
 function genkey
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout key.pem -out cert.pem
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=localhost" -keyout key.pem -out cert.pem
 end
