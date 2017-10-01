35ed664e86f5a30664c5e922dc2d156be20f47a3
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-10-01 22:18:47 +0200

    Remove neobundle setup
    Now using vim-plug, which will self-setup due to config in vimrc

--- nonbrew.bash
+++ nonbrew.bash
@@ -6,9 +6,6 @@ if [[ $? -ne 0 ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install neobundle for vim
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
 # install command-line tools
 xcode-select --install
 
