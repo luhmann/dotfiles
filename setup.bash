25cb9098b61d51bf8c2a67acd0304fbf854e4d8b
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2019-02-05 12:44:37 +0100

    feat: configure hammerspoon on install

--- setup.bash
+++ setup.bash
@@ -28,6 +28,8 @@ function setupHammerspoon() {
   # open -a Hammerspoon - open hammerspoon if menu-item and dock-item are disabled
   # cmd + , -- open preferences
   defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.hammerspoon/init.lua"
+  defaults write org.hammerspoon.Hammerspoon MJShowDockIconKey 0
+  defaults write org.hammerspoon.Hammerspoon MJShowMenuIconKey 0
 }
 
 installFonts
