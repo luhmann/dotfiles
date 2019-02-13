5f0256ffbdc83ed92f5905d8651c21cf1577757f
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2019-02-13 16:32:52 +0100

    feat: fix shortcut for locking screen, add shortcut for muting audio

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,9 +4,19 @@ calt = {"cmd", "ctrl"}
 require "spoons"
 require "apps"
 
+local volume = require "volume"
+
+
+local function sleepScreen()
+  return hs.execute("pmset displaysleepnow")
+end
+hs.caffeinate.sleepScreen = sleepScreen
 
 hs.hotkey.bind(mash, "r", function() hs.reload(); end)
-hs.hotkey.bind(mash, "l", function() hs.caffeinate.lockScreen(); end)
+hs.hotkey.bind(mash, "l", function() hs.caffeinate.sleepScreen(); end)
 -- defeat paste-blocks
-hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+hs.hotkey.bind({"cmd", "alt"}, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+-- mute
+hs.hotkey.bind(calt, "o", volume.mute)
+
 hs.alert("Hammerspoon config loaded")
